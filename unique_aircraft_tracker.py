4ab1206b148fe073138c18e7ccb389794fcdd169
Gristle King <124638769+gristlekinginc@users.noreply.github.com>
2023-11-19 21:06:30 -0800

    Update unique_aircraft_tracker.py
    made it so you only have to change the time period (in minutes) at the top of the file rather than throughout.

--- unique_aircraft_tracker.py
+++ unique_aircraft_tracker.py
@@ -1,16 +1,19 @@
 import json
 
+# Define the Time Period for Analysis
+TIME_PERIOD_MINUTES = 15  # Change this value to alter the time period
+
 # Read and parse the JSON data
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-# Define the Time Period for Analysis and Filter Aircraft
-def filter_data_by_time(data, minutes=15):
+# Filter Aircraft based on defined time period
+def filter_data_by_time(data):
     recent_aircraft = []
     for aircraft in data['aircraft']:
-        if 'seen' in aircraft and aircraft['seen'] <= minutes * 60:
+        if 'seen' in aircraft and aircraft['seen'] <= TIME_PERIOD_MINUTES * 60:
             recent_aircraft.append(aircraft)
     return recent_aircraft
 
@@ -29,7 +32,6 @@ def find_unique_aircraft_and_ranges(aircraft_list):
 def calculate_range_stats(ranges):
     if not ranges:
         return 0, 0
-    # Convert ranges to nautical miles
     ranges_nautical_miles = [r * 0.539957 for r in ranges]
     max_range = max(ranges_nautical_miles)
     avg_range = sum(ranges_nautical_miles) / len(ranges_nautical_miles)
@@ -40,8 +42,8 @@ def main():
     file_path = '/run/readsb/aircraft.json'  # Updated file path
     data = read_json_file(file_path)
 
-    # Filter data for the last 15 minutes (or your desired period)
-    recent_aircraft = filter_data_by_time(data, minutes=15)
+    # Filter data for the specified time period
+    recent_aircraft = filter_data_by_time(data)
 
     # Find unique aircraft and ranges
     unique_aircraft, ranges = find_unique_aircraft_and_ranges(recent_aircraft)
@@ -49,7 +51,7 @@ def main():
     # Calculate maximum and average range
     max_range, avg_range = calculate_range_stats(ranges)
 
-    print(f"Unique aircraft in the last 15 minutes: {len(unique_aircraft)}")
+    print(f"Unique aircraft in the last {TIME_PERIOD_MINUTES} minutes: {len(unique_aircraft)}")
     print(f"Maximum range seen: {max_range:.1f} nautical miles")
     print(f"Average range seen: {avg_range:.1f} nautical miles")
 
