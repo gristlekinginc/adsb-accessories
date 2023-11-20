0628c42011f767d8b1b418abf0096f325d6fffd7
Gristle King <124638769+gristlekinginc@users.noreply.github.com>
2023-11-19 21:03:52 -0800

    Update unique_aircraft_tracker.py
    added in max range and average range for the time period run

--- unique_aircraft_tracker.py
+++ unique_aircraft_tracker.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import json
 
 # Read and parse the JSON data
@@ -15,13 +14,26 @@ def filter_data_by_time(data, minutes=15):
             recent_aircraft.append(aircraft)
     return recent_aircraft
 
-# Identify Unique Aircraft
-def find_unique_aircraft(aircraft_list):
+# Identify Unique Aircraft and Calculate Ranges
+def find_unique_aircraft_and_ranges(aircraft_list):
     unique_hex_codes = set()
+    ranges = []
     for aircraft in aircraft_list:
         if 'hex' in aircraft:
             unique_hex_codes.add(aircraft['hex'])
-    return unique_hex_codes
+            if 'r_dst' in aircraft:
+                ranges.append(aircraft['r_dst'])
+    return unique_hex_codes, ranges
+
+# Calculate Maximum and Average Range
+def calculate_range_stats(ranges):
+    if not ranges:
+        return 0, 0
+    # Convert ranges to nautical miles
+    ranges_nautical_miles = [r * 0.539957 for r in ranges]
+    max_range = max(ranges_nautical_miles)
+    avg_range = sum(ranges_nautical_miles) / len(ranges_nautical_miles)
+    return max_range, avg_range
 
 # Main Function
 def main():
@@ -31,11 +43,15 @@ def main():
     # Filter data for the last 15 minutes (or your desired period)
     recent_aircraft = filter_data_by_time(data, minutes=15)
 
-    # Find unique aircraft
-    unique_aircraft = find_unique_aircraft(recent_aircraft)
+    # Find unique aircraft and ranges
+    unique_aircraft, ranges = find_unique_aircraft_and_ranges(recent_aircraft)
+
+    # Calculate maximum and average range
+    max_range, avg_range = calculate_range_stats(ranges)
+
     print(f"Unique aircraft in the last 15 minutes: {len(unique_aircraft)}")
-    # print(unique_aircraft)
+    print(f"Maximum range seen: {max_range:.1f} nautical miles")
+    print(f"Average range seen: {avg_range:.1f} nautical miles")
 
 if __name__ == "__main__":
     main()
-
