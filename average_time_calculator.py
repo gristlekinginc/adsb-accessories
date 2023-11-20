13c319d57846298c1080f0293971aba433c1567f
Gristle King <124638769+gristlekinginc@users.noreply.github.com>
2023-11-19 17:29:31 -0800

    Update average_time_calculator.py
    updated a bunch of stuff so it prints out diagnostics

--- average_time_calculator.py
+++ average_time_calculator.py
@@ -7,46 +7,49 @@ def read_json_file(file_path):
     return data
 
 def calculate_average_period(aircraft_data, target_flights=100):
-    # Current time based on the "now" field in the JSON data
     current_time = datetime.fromtimestamp(aircraft_data['now'])
+    aircraft_list = aircraft_data['aircraft']
 
-    # Filter aircraft with at least 2 messages
-    filtered_aircraft = [ac for ac in aircraft_data['aircraft'] if ac.get('messages', 0) >= 2]
+    # Diagnostic information
+    total_aircraft = len(aircraft_list)
+    unique_aircraft = len(set(ac['hex'] for ac in aircraft_list if 'hex' in ac))
+    min_seen = min(aircraft_list, key=lambda x: x.get('seen', float('inf')))['seen']
+    max_seen = max(aircraft_list, key=lambda x: x.get('seen', float('-inf')))['seen']
 
-    # Sort by the 'seen' time (most recently seen first)
-    filtered_aircraft.sort(key=lambda x: x['seen'])
+    # Time period calculation
+    earliest_time = current_time - timedelta(seconds=max_seen) if max_seen is not None else None
+    time_period_length_minutes = (current_time - earliest_time).total_seconds() / 60 if earliest_time else 0
 
+    filtered_aircraft = [ac for ac in aircraft_list if ac.get('messages', 0) >= 2]
     total_period = timedelta(0)
     count = 0
 
     for ac in filtered_aircraft:
         if count >= target_flights:
             break
-        # Calculate the time when the aircraft was last seen
         last_seen_time = current_time - timedelta(seconds=ac['seen'])
         total_period += (current_time - last_seen_time)
         count += 1
 
-    # Calculate average period in minutes
     if count > 0:
         average_period = total_period / count
-        return average_period.total_seconds() / 60  # Convert to minutes
+        average_period_minutes = average_period.total_seconds() / 60
     else:
-        return 0
+        average_period_minutes = 0
+
+    # Print diagnostic information
+    print(f"Total aircraft processed: {total_aircraft}")
+    print(f"Time period: {earliest_time.strftime('%Y-%m-%d %H:%M:%S')} to {current_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f"Length of time period: {time_period_length_minutes:.3f} minutes")
+    print(f"Number of unique aircraft: {unique_aircraft}")
+    print(f"Minimum 'seen' value: {min_seen} seconds")
+    print(f"Maximum 'seen' value: {max_seen} seconds")
+    print(f"Average time period for receiving at least 2 messages from 100 unique flights: {average_period_minutes:.1f} minutes")
 
 def main():
     file_path = '/run/readsb/aircraft.json'
     aircraft_data = read_json_file(file_path)
-    avg_period = calculate_average_period(aircraft_data)
-
-    # Check if the average time period is less than a minute
-    if avg_period < 1:
-        print("Average time period for receiving at least 2 messages from 100 unique flights: Less than 1 minute")
-    else:
-        formatted_avg_period = f"{avg_period:.1f}"
-        print(f"Average time period for receiving at least 2 messages from 100 unique flights: {formatted_avg_period} minutes")
+    calculate_average_period(aircraft_data)
 
 if __name__ == "__main__":
     main()
-
-
